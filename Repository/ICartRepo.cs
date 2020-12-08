a7454eba3e50ce4cb12db7ed95f13812ac132f18
Boddu Venkata Sai Ganesh <40751093+saiganesh1212@users.noreply.github.com>
2020-12-08 13:36:48 +0530

    Initial commit

---
+++ Repository/ICartRepo.cs
@@ -0,0 +1,15 @@
+﻿using CartService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CartService.Repository
+{
+    public interface ICartRepo
+    {
+        bool Add_Item_Into_Cart(int userId, MenuItem menuItem);
+        bool Remove_Item_From_Cart(int userId, int menuItemId);
+        List<MenuItem> Get_Cart_Of_User(int userId);
+    }
+}
